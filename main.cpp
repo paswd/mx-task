9e9e16ebfbb4c6a6071057331eec1907bb551ca6
paswd <paswd@fileserver.pwd.local>
2017-05-26 22:51:04 +0300

    Added position structure & input file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct tpoint_r {
+	double X;
+	double Y;
+
+	tpoint_r(void) {
+		this->X = 0.;
+		this->Y = 0.;
+	}
+};
+
+int main(void) {
+	return 0;
+}
